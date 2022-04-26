398a9391c68602fcfe9d5863249437665dbccfdf
Juan Andres Gutierrez <expatico506@gmail.com>
2022-04-25 21:03:32 -0600

    Added grid and profile photo

--- src/Components/Landing-Page/LandingPage.js
+++ src/Components/Landing-Page/LandingPage.js
@@ -1,22 +1,18 @@
 import "./Landing-Page.css";
 import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
-import crbeach from "../../images/crbeach.jpg";
-import memoji from "../../images/memoji.png";
-
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Image from "react-bootstrap/Image";
+import profile from "../../images/profile.png";
 const LandingPage = () => {
   return (
-    <Container
-      className="landing-page-container"
-      style={{ backgroundImage: `url(${crbeach})`, backgroundSize: "cover" }}
-    >
-        <Container className="my-memoji"><img src={memoji}/></Container>
-      <Container className="name-container">
-        <h1 className="landing-page-text">
-          Hi!, I'm Juan Andres, Full-Stack Software Engineer
-        </h1>
-      </Container>
-      <Button>Contact Me!</Button>
+    <Container>
+      <Row>
+        <Col lg={true}>
+        <Image fluid src={profile} />
+        </Col>
+        <Col>Introduction goes here</Col>
+      </Row>
     </Container>
   );
 };
