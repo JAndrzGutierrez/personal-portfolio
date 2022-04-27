5252529621a3da1238c0eca948d6ac0f687d4e4b
Juan Andres Gutierrez <expatico506@gmail.com>
2022-04-26 20:05:29 -0600

    added MyProjects component

--- src/Components/Landing-Page/LandingPage.js
+++ src/Components/Landing-Page/LandingPage.js
@@ -4,16 +4,27 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 import profile from "../../images/profile.png";
+import MyProjects from "../../Components/MyProjects/MyProjects.js";
 const LandingPage = () => {
   return (
     <Container>
       <Row>
         <Col className="profile-picture" lg={true}>
-        <Image fluid src={profile} width={450}/>
+          <Image fluid src={profile} width={450} />
         </Col>
-        <Col className="profile-intro"><h1>Hi, my name is Juan Andres!</h1>
-        <h3>I'm a Bilingual Full-Stack Software Engineer from Costa Rica, based out of <strong>Richmond, VA.</strong> I'm pasioned about learning <strong>new technologies</strong> and all the diffents ways that can be apply to create cool applications.</h3></Col>
+        <Col className="profile-intro">
+          <h1>Hi, my name is Juan Andres!</h1>
+          <h3>
+            I'm a Bilingual Full-Stack Software Engineer from Costa Rica, based
+            out of <strong>Richmond, VA.</strong> I'm pasioned about learning{" "}
+            <strong>new technologies</strong> and all the diffents ways that can
+            be apply to create cool applications.
+          </h3>
+        </Col>
       </Row>
+      <Col className="my-projects">
+        <MyProjects />
+      </Col>
     </Container>
   );
 };
---
+++ src/Components/MyProjects/MyProjects.js
@@ -0,0 +1,23 @@
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+
+
+const MyProjects = () => {
+    return (
+        <Container>
+            <Row>
+                <Col>Project 1</Col>
+                <Col>Project 2</Col>
+            </Row>
+            <Row>
+                <Col>Project 3</Col>
+                <Col>Project 4</Col>
+            </Row>
+         Projects Go Here!!!! 
+        </Container>
+    )
+}
+
+export default MyProjects;
