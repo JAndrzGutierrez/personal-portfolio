cdac84a8d584973024613ae67d6d354493bc4b4f
Juan Andres Gutierrez <expatico506@gmail.com>
2022-05-13 17:41:43 -0400

    added SkillBars to About section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,16 +1,49 @@
 import Container from "react-bootstrap/Container";
-
+import SkillBar from "react-skillbars";
 const AboutSection = () => {
+  const skills = [
+    { type: "Bash", level: 85 },
+    { type: "React", level: 75 },
+    { type: "Javascript", level: 75 },
+    { type: "HTML/CSS", level: 85 },
+    { type: "Python", level: 80 },
+    { type: "Django", level: 65 },
+    { type: "Postgres", level: 65 },
+    { type: "SQL", level: 75 },
+  ];
+  const colors = {
+    bar: "#519e8aff",
+    title: {
+      text: "#519e8aff",
+      background: "#020202ff",
+    },
+  };
+
   return (
     <Container>
       <h2>About Section</h2>
-    <h3>Hi, I'm Juan Andres!</h3>
-    <p>
-    As an adept Full-Stack Software Engineer, I am well-rounded in software development with honed talents in software engineering and software testing. In addition, I have specific experience with the following languages, frameworks and databases: React, javaScript, Python, HTML/CSS, API’s, CRUD, Postgres, Django, Heroku, Git and more. I am also a very fast learner and flourish in a team environment. My previous roles have strengthened my capabilities in problem-solving, scientific thinking and motivation, including a keen attention to detail and accuracy. 
-    </p>
-    
-    <h3>Contact</h3>
-   </Container>
+
+      <h3>Hi, I'm Juan Andres!</h3>
+      <p>
+        As an adept Full-Stack Software Engineer, I am well-rounded in software
+        development with honed talents in software engineering and software
+        testing. In addition, I have specific experience with the following
+        languages, frameworks and databases: React, javaScript, Python,
+        HTML/CSS, API’s, CRUD, Postgres, Django, Heroku, Git and more. I am also
+        a very fast learner and flourish in a team environment. My previous
+        roles have strengthened my capabilities in problem-solving, scientific
+        thinking and motivation, including a keen attention to detail and
+        accuracy.
+      </p>
+      <Container>
+        <SkillBar
+          skills={skills}
+          colors={colors}
+          offset={`25px`}
+          animationDuration={6500}
+        />
+      </Container>
+    </Container>
   );
 };
 export default AboutSection;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,30 +1,18 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import "./Header.css";
 
 const Header = () => {
   return (
-    <Navbar expand="lg" className="Navbar">
+    <Navbar expand="md" className="Navbar">
       <Container>
         <Navbar.Brand href="/">THE "CODEBELOW"</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">Contact</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="/about">About</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
